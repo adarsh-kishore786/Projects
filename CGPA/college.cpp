67dc1926ab4a49edd68cc95c081d15bb8ce0cc78
Adarsh Kishore <adarsh.kishore786@gmail.com>
2023-12-02 18:15:30 +0530

    College implemented as vector of semesters

---
+++ CGPA/college.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "college.h"
+#include "semester.h"
+
+College::College(std::string& collegeName) {
+  this->collegeName = collegeName;
+  inputSemesters();
+}
+
+std::string College::getCollegeName() const {
+  return collegeName;
+}
+
+std::vector<Semester> College::getSemesters() const {
+  return semesters;
+}
+
+void College::inputSemesters() {
+  std::string inputUrl = "grades/";
+  int numSemesters = 5;
+
+  for (int i = 1; i <= numSemesters; i++) {
+    auto sem = Semester();
+    auto fileUrl = inputUrl + (char)(i+48);
+    sem.inputCourses(fileUrl);
+
+    semesters.push_back(sem);
+  }
+}
+
+std::ostream& operator<<(std::ostream& os, const College& college) {
+  os << college.getCollegeName() << "\n\n";
+
+  for (auto& sem : college.getSemesters()) {
+    os << sem << "\n";
+  }
+  return os;
+}
---
+++ CGPA/college.h
@@ -0,0 +1,24 @@
+#ifndef __COLLEGE_H_INCLUDED__
+#define __COLLEGE_H_INCLUDED__
+
+#include <string>
+#include <vector>
+#include "semester.h"
+
+class College {
+private:
+  std::string collegeName;
+  std::vector<Semester> semesters;
+
+  void inputSemesters();
+
+public:
+  College(std::string& collegeName);
+
+  std::string getCollegeName() const;
+  std::vector<Semester> getSemesters() const;
+
+  friend std::ostream& operator<<(std::ostream& os, const College& college);
+};
+
+#endif

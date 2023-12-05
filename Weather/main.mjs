8d9279b637e7e16cbd466642d6253c21505696a9
Adarsh Kishore <adarsh.kishore786@gmail.com>
2023-12-05 17:28:45 +0530

    Hello express

--- Weather/main.mjs
+++ Weather/main.mjs
@@ -1,1 +1,13 @@
-console.log("Hello, World!")
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  console.log("hello");
+  res.send("hello");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on localhost:${PORT}`);
+});

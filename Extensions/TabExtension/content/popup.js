3ef23d671a5b50b346305d8431ec1aff3ec48068
Adarsh Kishore <adarsh.kishore786@gmail.com>
2024-02-05 10:03:35 +0530

    Refactored some more

--- Extensions/TabExtension/content/popup.js
+++ Extensions/TabExtension/content/popup.js
@@ -5,6 +5,7 @@ async function groupTabs(tabIds) {
 
 async function groupUngroupTabs(tabs) {
   const tabIds = tabs.map(({ id }) => id);
+
   if (!tabIds.length) {
     return;
   }
@@ -28,36 +29,47 @@ async function groupUngroupTabs(tabs) {
   await chrome.tabs.ungroup(tabIds);
   console.log(tabs);
 }
-const tabs = await chrome.tabs.query({
-  url: [
-    "https://developer.chrome.com/docs/webstore/*",
-    "https://developer.chrome.com/docs/extensions/*",
-    // "https://*/*",
-    // "http://*/*",
-  ]
-})
 
-const collator = new Intl.Collator();
-tabs.sort((a, b) => collator.compare(a.title, b.title));
+async function getTabs(urls) {
+  const tabs = await chrome.tabs.query({url: urls})
 
-const template = document.getElementById("li_template");
-const elements = new Set();
-for (const tab of tabs) {
-  const element = template.content.firstElementChild.cloneNode(true);
-
-  const title = tab.title.split("-")[0].trim();
-  const pathname = new URL(tab.url).pathname.slice("/docs".length);
-
-  element.querySelector(".title").textContent = title;
-  element.querySelector(".pathname").textContent = pathname;
-  element.querySelector("a").addEventListener("click", async () => {
-    await chrome.tabs.update(tab.id, { active: true });
-    await chrome.windows.update(tab.windowId, { focused: true });
-  });
-
-  elements.add(element);
+  const collator = new Intl.Collator();
+  tabs.sort((a, b) => collator.compare(a.title, b.title));
+  return tabs;
 }
-document.querySelector("ul").append(...elements);
 
-const button = document.querySelector("button");
-button.addEventListener("click", () => groupUngroupTabs(tabs));
+async function setUpHTML(urls) {
+  const tabs = await getTabs(urls);
+  const template = document.getElementById("li_template");
+  const elements = new Set();
+
+  for (const tab of tabs) {
+    const element = template.content.firstElementChild.cloneNode(true);
+
+    const title = tab.title.split("-")[0].trim();
+    const pathname = new URL(tab.url).pathname.slice("/docs".length);
+
+    element.querySelector(".title").textContent = title;
+    element.querySelector(".pathname").textContent = pathname;
+    element.querySelector("a").addEventListener("click", async () => {
+      await chrome.tabs.update(tab.id, { active: true });
+      await chrome.windows.update(tab.windowId, { focused: true });
+    });
+
+    elements.add(element);
+  }
+
+  document.querySelector("ul").append(...elements);
+
+  const button = document.querySelector("button");
+  button.addEventListener("click", () => groupUngroupTabs(tabs));
+}
+
+const urls = [
+  "https://developer.chrome.com/docs/webstore/*",
+  "https://developer.chrome.com/docs/extensions/*",
+  // "https://*/*",
+  // "http://*/*",
+]
+
+setUpHTML(urls);

ba3909965348795145d6f2829a5421a8bd7e79b0
Adarsh Kishore <adarsh.kishore786@gmail.com>
2025-08-24 16:58:38 +0530

    REST call made in Rust

--- Hackattic/HelpMeUnpack/src/main.rs
+++ Hackattic/HelpMeUnpack/src/main.rs
@@ -1,3 +1,20 @@
+use reqwest::get;
+use tokio::runtime::Runtime;
+
+const URL: &str = "https://hackattic.com/challenges/help_me_unpack/problem?access_token=be014966b3e84380";
+
+async fn get_problem() -> String {
+    let response = get(URL)
+        .await
+        .expect("Some error occured!");
+
+    return response.text().await.expect("Some error occured!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rt = Runtime::new().unwrap();
+
+    let problem = rt.block_on(get_problem());
+
+    println!("{problem}");
 }

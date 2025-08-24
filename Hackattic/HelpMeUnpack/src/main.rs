4d3f84ad47a85b3e77e4c01e417af889521d757f
Adarsh Kishore <adarsh.kishore786@gmail.com>
2025-08-24 22:47:10 +0530

    Reading through environment files is better

--- Hackattic/HelpMeUnpack/src/main.rs
+++ Hackattic/HelpMeUnpack/src/main.rs
@@ -1,20 +1,25 @@
 use reqwest::get;
 use tokio::runtime::Runtime;
+use std::env;
+use dotenv::dotenv;
 
-const URL: &str = "https://hackattic.com/challenges/help_me_unpack/problem?access_token=be014966b3e84380";
+const URL: &str = "https://hackattic.com/challenges/help_me_unpack/problem?access_token=";
 
-async fn get_problem() -> String {
-    let response = get(URL)
+async fn get_problem(access_token: &str) -> String {
+    let response = get(URL.to_owned() + access_token)
         .await
-        .expect("Some error occured!");
+        .expect("Some error occured while reading response!");
 
-    return response.text().await.expect("Some error occured!");
+    return response.text().await.expect("Some error occured while parsing the response!");
 }
 
 fn main() {
+    let _ = dotenv();
+
+    let access_token = env::var("ACCESS_TOKEN").expect("Couldn't find the access token in environment file!");
     let rt = Runtime::new().unwrap();
 
-    let problem = rt.block_on(get_problem());
+    let problem = rt.block_on(get_problem(&access_token));
 
     println!("{problem}");
 }

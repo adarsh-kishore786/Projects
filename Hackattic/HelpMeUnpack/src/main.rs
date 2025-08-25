7aa3266bafb8974a7871570179e8abf37f401392
Adarsh Kishore <adarsh.kishore786@gmail.com>
2025-08-25 23:05:14 +0530

    Understanding ownership and strings

--- Hackattic/HelpMeUnpack/src/main.rs
+++ Hackattic/HelpMeUnpack/src/main.rs
@@ -5,8 +5,8 @@ use dotenv::dotenv;
 
 const URL: &str = "https://hackattic.com/challenges/help_me_unpack/problem?access_token=";
 
-async fn get_problem(access_token: &str) -> String {
-    let response = get(URL.to_owned() + access_token)
+async fn get_problem(access_token: String) -> String {
+    let response = get(format!("{URL}{access_token}"))
         .await
         .expect("Some error occured while reading response!");
 
@@ -16,10 +16,10 @@ async fn get_problem(access_token: &str) -> String {
 fn main() {
     let _ = dotenv();
 
-    let access_token = env::var("ACCESS_TOKEN").expect("Couldn't find the access token in environment file!");
+    let access_token: String = env::var("ACCESS_TOKEN").expect("Couldn't find the access token in environment file!");
     let rt = Runtime::new().unwrap();
 
-    let problem = rt.block_on(get_problem(&access_token));
+    let problem = rt.block_on(get_problem(access_token));
 
     println!("{problem}");
 }

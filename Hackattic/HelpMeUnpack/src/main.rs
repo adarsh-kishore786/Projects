85d73e34161a79be99f59b94f5abcbcb1769cd1f
Adarsh Kishore <adarsh.kishore786@gmail.com>
2025-08-25 23:23:46 +0530

    Converting response to Rust struct and deserialized it

--- Hackattic/HelpMeUnpack/src/main.rs
+++ Hackattic/HelpMeUnpack/src/main.rs
@@ -2,8 +2,16 @@ use reqwest::get;
 use tokio::runtime::Runtime;
 use std::env;
 use dotenv::dotenv;
+use serde::Deserialize;
+use serde_json::from_str;
 
 const URL: &str = "https://hackattic.com/challenges/help_me_unpack/problem?access_token=";
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Problem {
+    bytes: String
+}
 
 async fn get_problem(access_token: String) -> String {
     let response = get(format!("{URL}{access_token}"))
@@ -19,7 +27,9 @@ fn main() {
     let access_token: String = env::var("ACCESS_TOKEN").expect("Couldn't find the access token in environment file!");
     let rt = Runtime::new().unwrap();
 
-    let problem = rt.block_on(get_problem(access_token));
+    let response: &str = &rt.block_on(get_problem(access_token));
+    let problem_struct: Problem = from_str(response).expect("The response is not in the format expected!");
+    let problem_bytes: String = problem_struct.bytes;
 
-    println!("{problem}");
+    println!("{problem_bytes}");
 }

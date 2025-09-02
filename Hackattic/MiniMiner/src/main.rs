1d99550a17eea013335b1cd2242f3a6e6e3d3122
Adarsh Kishore <adarsh.kishore786@gmail.com>
2025-09-02 23:54:43 +0530

    Got the problem statement through REST call

--- Hackattic/MiniMiner/src/main.rs
+++ Hackattic/MiniMiner/src/main.rs
@@ -2,17 +2,48 @@ use reqwest;
 use tokio;
 use std::env;
 use dotenv::dotenv;
+use serde::Deserialize;
 
 const URL: &str = "https://hackattic.com/challenges/mini_miner";
 
-async fn get_problem(client: &reqwest::Client, access_token: &str) -> String {
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Data {
+    hex: String,
+    int: i32
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Block {
+    nonce: Option<i32>,
+    data: Vec<Data>
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Problem {
+    difficulty: i32,
+    block: Block
+}
+
+async fn get_problem(client: &reqwest::Client, access_token: &str) -> Problem {
     let response = client
         .get(format!("{URL}/problem?access_token={access_token}"))
         .send()
         .await
         .expect("Some error occured while fetching client response!");
 
-    return response.text().await.expect("Some error occured while parsing the response!");
+    let problem: Problem = response.json().await.expect("Some error occured while parsing the response!");
+
+    return problem;
+}
+
+fn process(problem: &Problem) {
+    let difficulty = problem.difficulty;
+    let block: &Block = &problem.block;
+
+    println!("{block:?}");
 }
 
 #[tokio::main]
@@ -26,5 +57,5 @@ async fn main() {
                  .expect("Not able to initialize client!");
 
     let problem = get_problem(&client, &access_token).await;
-    println!("{problem:?}");
+    process(&problem);
 }

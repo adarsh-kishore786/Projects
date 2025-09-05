dc216002416aa05cef12edae3f8dcf42a565c3fd
Adarsh Kishore <adarsh.kishore786@gmail.com>
2025-09-05 23:31:08 +0530

    RESTful GET to URL endpoint is successful

--- Hackattic/PasswordHashing/main.go
+++ Hackattic/PasswordHashing/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "log"
+  "os"
+  "net/http"
+  "io/ioutil"
+
+  "github.com/joho/godotenv"
+)
+
+const URL = "https://hackattic.com/challenges/password_hashing";
+
+func GetProblem(accessToken string) string {
+  response, err := http.Get(URL + "/problem?access_token=" + accessToken)
+
+  if err != nil {
+    fmt.Println(err.Error())
+    os.Exit(1)
+  }
+
+  responseData, err := ioutil.ReadAll(response.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  return string(responseData)
+}
 
 func main()  {
-  fmt.Println("Hello, world!")
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Could not find .env file!")
+  }
+
+  accessToken := os.Getenv("ACCESS_TOKEN")
+
+  fmt.Println(GetProblem(accessToken))
 }

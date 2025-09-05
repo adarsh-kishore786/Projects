db0882bc9771986d8a8fe36209373a05122797ba
Adarsh Kishore <adarsh.kishore786@gmail.com>
2025-09-05 21:40:52 +0530

    Added support for reading a file

--- CodingChallenges/wc/Main.java
+++ CodingChallenges/wc/Main.java
@@ -1,7 +1,36 @@
 package wc;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static String readFile(String filePath) throws IOException {
+    try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+
+      StringBuilder text = new StringBuilder();
+      String line = reader.readLine();
+
+      while (line != null) {
+        text.append(line);
+        text.append(System.lineSeparator());
+        line = reader.readLine();
+      }
+      return text.toString();
+
+    } catch (IOException e) {
+      throw new IOException(e);
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    if (args.length < 1) {
+      System.err.println("Supply a file to be counted!");
+      System.exit(1);
+    }
+
+    String text = readFile(args[0]);
+
+    System.out.println(text.split("\n")[0]);
   }
 }

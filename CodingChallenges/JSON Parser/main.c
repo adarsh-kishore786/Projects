1ce71a0430f7c851794ea2669a9b85789c0e95a1
Adarsh Kishore <adarsh.kishore786@gmail.com>
2025-09-07 15:39:27 +0530

    Added support for printing file contents

--- CodingChallenges/JSON Parser/main.c
+++ CodingChallenges/JSON Parser/main.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  printf("Hello, world!\n"); }
+void readFile(const char *filePath) {
+  char ch;
+
+  FILE *fptr = fopen(filePath, "r");
+
+  while ((ch = fgetc(fptr)) != EOF)
+    printf("%c", ch);
+  printf("\n");
+
+  fclose(fptr);
+}
+
+int main(int argc, const char **argv) {
+  if (argc < 2) {
+    printf("Usage: ./parse <file-name>\n");
+    exit(0);
+  }
+
+  readFile(argv[1]);
+}

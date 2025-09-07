246ee2944ee36df1cfc082c1d5032951bf5a3646
Adarsh Kishore <adarsh.kishore786@gmail.com>
2025-09-07 15:59:20 +0530

    File absence handled gracefully

--- CodingChallenges/JSON Parser/main.c
+++ CodingChallenges/JSON Parser/main.c
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-void readFile(const char *filePath) {
+void getFileSize(FILE *fptr) {
+  int prev=ftell(fp);
+  fseek(fp, 0L, SEEK_END);
+  int sz=ftell(fp);
+  fseek(fp,prev,SEEK_SET); //go back to where we were
+
+  return sz;
+}
+
+char* readFile(const char *filePath) {
   char ch;
 
   FILE *fptr = fopen(filePath, "r");
@@ -12,11 +21,14 @@ void readFile(const char *filePath) {
     exit(1);
   }
 
-  while ((ch = fgetc(fptr)) != EOF)
-    printf("%c", ch);
-  printf("\n");
+  char text[getFileSize(fptr)];
+  int c = 0;
+
+  while ((ch = fgetc(fptr)) != EOF && c < len(text))
+    text[c++] = ch;
 
   fclose(fptr);
+  return text;
 }
 
 int main(int argc, const char **argv) {
@@ -25,5 +37,6 @@ int main(int argc, const char **argv) {
     exit(0);
   }
 
-  readFile(argv[1]);
+  char* text = readFile(argv[1]);
+  printf("%s\n", text);
 }

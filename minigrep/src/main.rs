16bd1393f9f836bbbd7dfa000f04eb0f5d31f898
Adarsh Kishore <adarsh.kishore786@gmail.com>
2025-09-12 12:43:30 +0530

    Better handling of config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,16 +1,28 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    file_path: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        return Config { query, file_path }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Query: {0}", config.query);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");

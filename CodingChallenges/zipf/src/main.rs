7b40f8d913f51622938f5f7d5254a1ed6de59cc5
adarsh-kishore786 <adarsh.kishore786@gmail.com>
2025-10-11 12:28:29 +0530

    Separated the file reading system as well
    Separated the file system

--- CodingChallenges/zipf/src/main.rs
+++ CodingChallenges/zipf/src/main.rs
@@ -1,39 +1,18 @@
 mod error;
+mod file;
 
 use error::Error;
 use std::env;
-use std::fs;
-use std::process;
-
-#[derive(Debug)]
-#[allow(dead_code)]
-struct File {
-    file_path: String,
-    contents: String
-}
-
-fn read_file(file_path: &str) -> File {
-    let contents = fs::read_to_string(file_path);
-
-    match contents {
-        Ok(s) => return File {
-            file_path: file_path.to_string(),
-            contents: s
-        },
-        Err(e) => {
-            println!("There was an error reading the file: {}", e);
-            process::exit(Error::ReadError as i32);
-        }
-    }
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        println!("Usage: ./zipf path/to/file");
-        process::exit(Error::NoArgs as i32);
+        error::exit(
+            "Usage: ./zipf path/to/file",
+            Error::NoArgs 
+        );
     }
 
-    let file = read_file(&args[1]);
+    let file = file::read_file(&args[1]);
     println!("{:?}", file);
 }

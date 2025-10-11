a86f411ae64e1026fde086d0dbbdc83896e2466d
adarsh-kishore786 <adarsh.kishore786@gmail.com>
2025-10-11 12:28:29 +0530

    Separated the file reading system as well

---
+++ CodingChallenges/zipf/src/file.rs
@@ -0,0 +1,27 @@
+mod error;
+
+use error::Error;
+use std::fs;
+use std::process;
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct File {
+    file_path: String,
+    contents: String
+}
+
+pub fn read_file(file_path: &str) -> File {
+    let contents = fs::read_to_string(file_path);
+
+    match contents {
+        Ok(s) => return File {
+            file_path: file_path.to_string(),
+            contents: s
+        },
+        Err(e) => {
+            println!("There was an error reading the file: {}", e);
+            process::exit(Error::ReadError as i32);
+        }
+    }
+}

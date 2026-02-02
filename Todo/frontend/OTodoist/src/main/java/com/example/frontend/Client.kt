61c86b51ac8189aef61e3ff3d780c353234c44ad
adarsh-kishore786 <adarsh.kishore786@gmail.com>
2026-02-02 19:56:32 +0530

    Made the client

--- Todo/frontend/OTodoist/src/main/java/com/example/frontend/Client.kt
+++ Todo/frontend/OTodoist/src/main/java/com/example/frontend/Client.kt
@@ -1,4 +1,27 @@
 package com.example.frontend
 
-class Client {
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Client {
+    private const val BASE_URL = "http://192.168.0.113:3000/"
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    val instance: Interface by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(Interface::class.java)
+    }
 }
